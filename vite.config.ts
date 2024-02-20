e50df3c2ac1c7f538796fa2ddb2883c06ea7bc62
Mai Thanh Phat <MaiThanhPhat>
2024-02-20 15:27:44 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   outDir: 'build', // Thay 'build' bằng thư mục bạn muốn xuất build
-  // },
+  build: {
+    outDir: 'build', // Thay 'build' bằng thư mục bạn muốn xuất build
+  },
 })
