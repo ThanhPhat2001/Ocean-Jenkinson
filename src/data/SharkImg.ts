29b6abff4a784750e51b02235aab9f71bbe960ca
Mai Thanh Phat <MaiThanhPhat>
2024-03-09 09:58:39 +0700

    update

--- src/data/SharkImg.ts
+++ src/data/SharkImg.ts
@@ -1,103 +1,105 @@
-// import Shark_Img1 from "../../public/images/SharkImg1.jpg";
-// import Shark_Img2 from "../../public/images/SharkImg1.jpg";
-// import Shark_Img3 from "../../public/images/SharkImg1.jpg";
-// import Shark_Img4 from "../../public/images/SharkImg1.jpg";
-// import Shark_Img5 from "../../public/images/SharkImg1.jpg";
-// import Shark_Img6 from "../../public/images/SharkImg1.jpg";
-// import Shark_Img6 from "../../public/images/SharkImg1.jpg";
-// import Shark_Img1 from "../../public/images/SharkImg1.jpg";
-// import Shark_Img1 from "../../public/images/SharkImg1.jpg";
-// import Shark_Img1 from "../../public/images/SharkImg1.jpg";
-
-
-
+import Shark_Img1 from "../../public/images/SharkImg1.jpg";
+import Shark_Img2 from "../../public/images/SharkImg2.jpg";
+import Shark_Img3 from "../../public/images/SharkImg3.jpg";
+import Shark_Img4 from "../../public/images/SharkImg4.jpg";
+import Shark_Img5 from "../../public/images/SharkImg5.jpg";
+import Shark_Img6 from "../../public/images/SharkImg6.jpg";
+import Shark_Img7 from "../../public/images/SharkImg7.jpg";
+import Shark_Img8 from "../../public/images/SharkImg8.jpg";
+import Shark_Img9 from "../../public/images/SharkImg9.jpg";
+import Shark_Img10 from "../../public/images/SharkImg10.jpg";
+import Shark_Img11 from "../../public/images/SharkImg11.jpg";
+import Shark_Img12 from "../../public/images/SharkImg12.jpg";
+import Shark_Img13 from "../../public/images/SharkImg13.jpg";
+import Shark_Img14 from "../../public/images/SharkImg14.jpg";
+import Shark_Img15 from "../../public/images/SharkImg15.jpg";
 
 export const SharksData = [
     {
       id: 1,
-      imageUrl: '../../public/images/SharkImg1.jpg',
+      imageUrl: Shark_Img1,
       title: 'Whale sharks in sunlight, Triton Bay, Indonesia',
       source: 'Janlue',
     },
     {
         id: 2,
-        imageUrl: "public/images/SharkImg2.jpg",
+        imageUrl: Shark_Img2,
         title: 'Ice-hunting sharks in Chiba, Japan',
         source: 'Martin Voeller',
       },
       {
         id: 3,
-        imageUrl: "public/images/SharkImg3.jpg",
+        imageUrl: Shark_Img3,
         title: 'Hammerhead shark below the surface, Malpelo (Colombia)',
         source: 'Piter',
       }, {
         id: 4,
-        imageUrl: "public/images/SharkImg4.jpg",
+        imageUrl: Shark_Img4,
         title: 'BLACKTIP REEF SHARK,Indian Ocean',
         source: 'Hubert Yann',
       }, {
         id: 5,
-        imageUrl: "public/images/SharkImg5.jpg",
+        imageUrl: Shark_Img5,
         title: ' Negaprion brevirostris,Pacific',
         source: 'Hubert Yann',
       },
       {
         id: 6,
-        imageUrl: "public/images/SharkImg6.jpg",
+        imageUrl: Shark_Img6,
         title: ' Shark and small fish in the ocean,Pacific',
         source: 'TPoPoVa',
       },
       {
         id: 7,
-        imageUrl: "public/images/SharkImg7.jpg",
+        imageUrl: Shark_Img7,
         title: ' Great hammerhead shark, Bimini, Bahamas',
         source: 'Nigel Marsh',
       },
       {
         id: 8,
-        imageUrl: "public/images/SharkImg8.jpg",
+        imageUrl: Shark_Img8,
         title: ' Silver Tip Shark',
         source: 'Atese',
       },
       {
         id: 9,
-        imageUrl: "public/images/SharkImg9.jpg",
+        imageUrl: Shark_Img9,
         title: 'Whale Sharks,Pemba ',
         source: 'Richcarey',
       },
       {
         id: 10,
-        imageUrl: "public/images/SharkImg10.jpg",
+        imageUrl: Shark_Img10,
         title: ' Lemon Shark,Pacific',
         source: 'Hubert Yann',
       },
       {
         id: 11,
-        imageUrl: "public/images/SharkImg11.jpg",
+        imageUrl: Shark_Img11,
         title: ' White-eyed Bull Shark,Ohio',
         source: 'Alsie',
       },
       {
         id: 12,
-        imageUrl: "public/images/SharkImg12.jpg",
+        imageUrl: Shark_Img12,
         title: ' Swimming Shark,Pacific',
         source: 'Husdn',
       },
       {
         id: 13,
-        imageUrl: "public/images/SharkImg13.jpg",
+        imageUrl: Shark_Img13,
         title: ' White Shark Underwater,Indian Ocean',
         source: 'Luese',
       },
       {
         id: 14,
-        imageUrl: "public/images/SharkImg14.jpg",
+        imageUrl: Shark_Img14,
         title: ' Whale Shark in the water',
         source: 'Alex',
       },
       {
         id: 15,
-        imageUrl: "public/images/SharkImg15.jpg",
+        imageUrl: Shark_Img15,
         title: ' Mackerel and great White Sharks',
         source: 'Hube-Sde',
       },
