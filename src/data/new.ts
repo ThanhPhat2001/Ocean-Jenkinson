28daf324e850a385c917a1ef6a0f749cc237992e
Mai Thanh Phat <MaiThanhPhat>
2024-03-09 09:23:05 +0700

    up

--- src/data/new.ts
+++ src/data/new.ts
@@ -1,27 +1,33 @@
+import new_img1 from "../../public/images/blog_image4.png";
+import new_img2 from "../../public/images/blog_image_content2.jpg";
+import new_img3 from "../../public/images/blog_image_content3.jpg";
+import new_img4 from "../../public/images/blog_image5.jpg";
+import new_img5 from "../../public/images/blog_image_content1.jpg";
+
 export const New = [
-    {
-        id: 1,
-        img: "./public/images/blog_image4.png",
-        title: "5 Ways Keeping Fish May Help Reduce Stress"
-    },
-    {
-        id: 2,
-        img: "./public/images/blog_image_content2.jpg",
-        title: "What can fish eat as treats?"
-    },
-    {
-        id: 3,
-        img: "./public/images/blog_image_content3.jpg",
-        title: "Do fish need protein?"
-    },
-    {
-        id: 4,
-        img: "./public/images/blog_image5.jpg",
-        title: "Why is my fish a picky eater? "
-    },
-    {
-        id: 5,
-        img: "./public/images/blog_image_content1.jpg",
-        title: "5 Awesome Ways Owning Fish Benefits Children"
-    }
-]
+  {
+    id: 1,
+    img: new_img1,
+    title: "5 Ways Keeping Fish May Help Reduce Stress",
+  },
+  {
+    id: 2,
+    img: new_img2,
+    title: "What can fish eat as treats?",
+  },
+  {
+    id: 3,
+    img: new_img3,
+    title: "Do fish need protein?",
+  },
+  {
+    id: 4,
+    img: new_img4,
+    title: "Why is my fish a picky eater? ",
+  },
+  {
+    id: 5,
+    img: new_img5,
+    title: "5 Awesome Ways Owning Fish Benefits Children",
+  },
+];
